721eb8cb81f39951e03729bb3f9306954cdd742b
hhh <hhhh>
2021-06-14 23:08:30 +0900

    위치 바꿈
    이때까지 한 커밋 다 날아감 ㅡㅡ

--- time_traker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_traker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -7,7 +7,14 @@ import 'package:time_traker_flutter_course/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _isLoading = false; //대기상태
+
   void _showSignInError(BuildContext context, Exception exception){
     if(exception is FirebaseException && exception.code == 'ERROR_ABORTED_BY_USER'){
       return;
@@ -22,28 +29,40 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously(BuildContext context) async{
     try{//에런 catch
+      setState(()=> _isLoading = true); //로딩
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInAnonymously();
     }on Exception catch(e){
       _showSignInError(context, e);
+    } finally{
+      setState(()=> _isLoading = false); //로딩 끝
     }
   }
+
   Future<void> _signInWithGoogle(BuildContext context) async{
     try{//에런 catch
+      setState(()=> _isLoading = true);
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInWithGoogle();
     }on Exception catch(e){
       _showSignInError(context, e);
+    }finally{
+      setState(()=> _isLoading = false); //로딩 끝
     }
   }
+
   Future<void> _signInWithFacebook(BuildContext context) async{
     try{//에런 catch
+      setState(()=> _isLoading = true);
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInWithFacebook();
     }on Exception catch(e){
       _showSignInError(context, e);
+    }finally{
+      setState(()=> _isLoading = false); //로딩 끝
     }
   }
+
   void _signInWithEmail(BuildContext context){
     Navigator.of(context).push( //푸시 내비게이션 스택
       MaterialPageRoute<void>(
@@ -65,6 +84,7 @@ class SignInPage extends StatelessWidget {
       backgroundColor: Colors.grey[200], //백그라운드 컬러
     );
   }
+
   Widget _buildContent(BuildContext context) { // _ 언더스코어는 private이 된다. 자기 파일에서만 접속가능
     return Padding( //Container대신 return 가능 단 색깔 지정 불가능, 바꿈에도 실행이 가능한 이유는 childe로 Colum을 가지고 있기 때문
       //body는 스카폴드의 하얀색 배경부분 거기에 컨테이너를 추가
@@ -75,20 +95,17 @@ class SignInPage extends StatelessWidget {
         crossAxisAlignment:
             CrossAxisAlignment.stretch, //children이 가로로 어떻게 나와야하는지 정함
         children: <Widget>[
-          Text('Sign in',//Container대신 Text 넣음음
-           textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 32.0,
-              fontWeight: FontWeight.w600,
-            ),
+          SizedBox(height: 50.0,
+              child: _buildHeader()
           ),
+
           SizedBox(height: 48.0),
           SocialSignInButton(
             assetName: 'images/google-logo.png',
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,
-            onPressed: ()=> _signInWithGoogle(context),
+            onPressed: _isLoading ? null : ()=> _signInWithGoogle(context),
           ),
           SizedBox(height: 8.0),
           SocialSignInButton(
@@ -96,14 +113,14 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Facebook',
             textColor: Colors.white,
             color: Color(0xFF334D92),
-            onPressed: ()=> _signInWithFacebook(context),
+            onPressed:_isLoading ? null : ()=> _signInWithFacebook(context),
           ),
           SizedBox(height: 8.0),
           SignInButton(
             text: 'Sign in with email',
             textColor: Colors.white,
             color: Colors.teal[700],
-            onPressed: () => _signInWithEmail(context),
+            onPressed:_isLoading ? null : () => _signInWithEmail(context),
           ),
           SizedBox(height: 8.0),
           Text(
@@ -116,10 +133,25 @@ class SignInPage extends StatelessWidget {
             text: 'Go anonymous',
             textColor: Colors.black,
             color: Colors.lime[300],
-            onPressed: ()=> _signInAnonymously(context),
+            onPressed:_isLoading ? null : ()=> _signInAnonymously(context),
           ),
 
         ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(){
+    if(_isLoading){
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Text('Sign in',//Container대신 Text 넣음음
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 32.0,
+        fontWeight: FontWeight.w600,
       ),
     );
   }
