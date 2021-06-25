3f1f3a4f9b24e88f27608d13830c2b8ba9306346
swh <sungwoo166@gmail.com>
2021-06-25 21:21:36 +0900

    sign_in_bloc.dart 로그인 함수 추가
    구글, 페이스북, 익명 로그인 추가

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 
