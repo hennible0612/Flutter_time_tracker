7ef9084284c87d668e8da832f466d0883b43fd02
swh <sungwoo166@gmail.com>
2021-06-27 22:22:21 +0900

    이메일싸인인Bloc
    submit추가

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,8 +1,12 @@
 
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:time_traker_flutter_course/app/sign_in/email_sign_in_model.dart';
+import 'package:time_traker_flutter_course/services/auth.dart';
 class EmailSignInBloc{
+  EmailSignInBloc({@required this.auth});
+  final AuthBase auth;
   final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
   EmailSignInModel _model = EmailSignInModel();
@@ -12,6 +16,19 @@ class EmailSignInBloc{
     _modelController.close();
   }
 
+  Future <void> submit() async{
+    updateWith(submitted: true, isLoading: true);
+    try{
+      if(_model.formType == EmailSignInFormType.signIn){//signIn상태라면
+        await auth.signInWithEmailAndPassword(_model.email, _model.password);//로그인
+      }else{
+        await auth.createUserWithEmailAndPassword(_model.email, _model.password);
+      }
+    }catch(e){
+      updateWith(isLoading: false);
+      rethrow;
+    }
+  }
   void updateWith({
   String email,
     String password,
