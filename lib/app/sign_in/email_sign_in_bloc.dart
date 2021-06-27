aaf18d3eb5a9f90e6bf53124f5d3cb59f8e5c663
swh <sungwoo166@gmail.com>
2021-06-27 18:38:46 +0900

    이메일싸인인모델, Bloc 수정 모델추가
    .

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -5,6 +5,7 @@ import 'package:time_traker_flutter_course/app/sign_in/email_sign_in_model.dart'
 class EmailSignInBloc{
   final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
+  EmailSignInModel _model = EmailSignInModel();
 
 
   void dispose(){
@@ -18,6 +19,13 @@ class EmailSignInBloc{
     bool isLoading,
     bool submitted,
   }){
-
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+    _modelController.add(_model);
   }
 }
