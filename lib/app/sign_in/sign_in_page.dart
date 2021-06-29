1f6262e14fc21369a996980472d6bcaec874c996
swh <sungwoo166@gmail.com>
2021-06-29 22:58:22 +0900

    ValueNotifier추가
    .

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -14,11 +14,14 @@ class SignInPage extends StatelessWidget {
   final SignInBloc bloc;
   static Widget create(BuildContext context){
     final auth = Provider.of<AuthBase>(context, listen: false);
-
-    return Provider<SignInBloc>( //parent 타입: SignInBloc
-      create: (_)=> SignInBloc(auth: auth),
-      dispose: (_,bloc)=> bloc.dispose(),
-      child: Consumer<SignInBloc>(builder: (_,bloc,__)=>SignInPage(bloc: bloc,)),
+    return ChangeNotifierProvider<ValueNotifier<bool>>(
+      create:(_) => ValueNotifier<bool>(false),
+      child: Consumer<ValueNotifier<bool>>(
+        builder: (_, isLoading,__)=> Provider<SignInBloc>( //parent 타입: SignInBloc
+          create: (_)=> SignInBloc(auth: auth, isLoading: isLoading),
+          child: Consumer<SignInBloc>(builder: (_,bloc,__)=>SignInPage(bloc: bloc,)),
+        ),
+      ),
     );
   }
 
@@ -65,23 +68,17 @@ class SignInPage extends StatelessWidget {
         builder: (context) => EmailSignInPage(), //EmailSignInPage리턴
       ),
     );
-
   }
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = Provider.of<ValueNotifier<bool>>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
         elevation: 2.0, // 앱바 그림자
       ),
-      body: StreamBuilder<bool>(
-        stream: bloc.isLoadingStream,
-        initialData: false,
-        builder: (context, snapshot) {
-          return _buildContent(context, snapshot.data);
-        }
-      ),
+      body: _buildContent(context, isLoading.value),
       backgroundColor: Colors.grey[200], //백그라운드 컬러
     );
   }
