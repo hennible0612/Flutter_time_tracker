21b0feefea855828ee124ed4de588e9a62c56d80
swh <sungwoo166@gmail.com>
2021-07-05 01:13:05 +0900

    database.dart에 job.dart추가
    .

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,17 +1,18 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:time_traker_flutter_course/home/models/job.dart';
 abstract class Database{
-  Future<void> createJob(Map<String, dynamic> jobData);
+  Future<void> createJob(Job job);
 }
 
 class FirestoreDatabase implements Database{
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
-  Future<void> createJob(Map<String, dynamic> jobData) async{
+  Future<void> createJob(Job job) async{
     final path = '/users/$uid/jobs/job_abc'; //위치
     final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(jobData); //위치에 데이터 넣음
+    await documentReference.set(job.toMap()); //위치에 데이터 넣음
   }
 }
