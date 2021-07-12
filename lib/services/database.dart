239a2b2a559bdfd94585c33cfb5d3a7bbde017d2
swh <sungwoo166@gmail.com>
2021-07-12 10:20:49 +0900

    database와 firestoreeservice 폴더 서로 분리
    firestore에 접속하는 함수는 firestoreservice 폴더로 이동

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,33 +3,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_traker_flutter_course/home/models/job.dart';
 import 'APIPath.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class Database{
   Future<void> createJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
-class FirestoreDatabase implements Database{
+class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
-  Future<void> createJob(Job job) => _setData(
-      path: APIPath.job(uid,'job_abc'),
-      data: job.toMap(),
-    );
+  Future<void> createJob(Job job) =>
+      _setData(
+        path: APIPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
 
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-          (snapshot) => Job.fromMap(snapshot.data()),
-    ).toList());
-  }
+  Stream<List<Job>> jobsStream() =>
+      _collectionStream(
+        path: APIPath.jobs(uid),
+        builder: (data) => Job.fromMap(data),
+      );
 
-  Future<void> _setData({String path, Map<String, dynamic>data}) async{
-    final reference = FirebaseFirestore.instance.doc(path);
-    print('$path: $data');
-    await reference.set(data);
-  }
+
 }
