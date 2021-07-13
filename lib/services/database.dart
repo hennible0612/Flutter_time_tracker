8b4052bc15d8da2648353935ba112bb7fd77bc33
swh <sungwoo166@gmail.com>
2021-07-13 19:46:07 +0900

    database firestoreservice 수정
    .

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,10 +1,10 @@
 //database.dart
 import 'package:flutter/cupertino.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_traker_flutter_course/home/models/job.dart';
 import 'APIPath.dart';
 import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
+
+import 'firestore_service.dart';
 
 abstract class Database{
   Future<void> createJob(Job job);
@@ -15,14 +15,16 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
 
+  final _service = FirestoreService.instance;
+
   Future<void> createJob(Job job) =>
-      _setData(
+      _service.setData(
         path: APIPath.job(uid, 'job_abc'),
         data: job.toMap(),
       );
 
   Stream<List<Job>> jobsStream() =>
-      _collectionStream(
+      _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data) => Job.fromMap(data),
       );
